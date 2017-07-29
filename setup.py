4dbf7a5af2fba3302b388daea8233ccce322fb73
Chris Vasquez <drchrisvasquez@gmail.com>
2017-07-30 08:39:28 +1000

    feat: Initial files to work with PyPI
    Added setup.py and setup.cfg to make the package to work with PYPI

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+  name = 'independentreserve',
+  packages = ['independentreserve'], # this must be the same as the name above
+  version = '0.1',
+  description = 'Python Client for Independent Reserve Web API',
+  author = 'Chris Vasquez',
+  author_email = 'cvsvasquez@gmail.com',
+  url = 'https://github.com/chrisvasquez/independentreserve-python', # use the URL to the github repo
+  download_url = 'https://github.com/chrisvasquez/independentreserve-python/archive/0.1.tar.gz', # I'll explain this in a second
+  keywords = ['api', 'client', 'independent reserve'], # arbitrary keywords
+  classifiers = [],
+)
